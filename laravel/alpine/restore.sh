d57f04f0a4a7a4a974aed327f9163877efd33f7d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-04-05 22:28:45 +0700

    backup and restore database

---
+++ laravel/alpine/restore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# current dir + /backup
+VOL_EXT=$(pwd)/backup
+
+# RESTORE DATABASE
+
+CONTAINER=alpine_db_1
+
+TEMP_CONTAINER=dbstore_container
+FILE_NAME=alpine_db.tar
+CON_DATA=/var/lib/postgresql/data
+VOL_NAME=alpine_db
+
+docker run -v $VOL_NAME:$CON_DATA --name $TEMP_CONTAINER alpine /bin/sh
+
+docker run --rm --volumes-from $TEMP_CONTAINER \
+    -v $VOL_EXT:/backup alpine \
+    /bin/sh -c "cd / && tar xvf /backup/$FILE_NAME"
+
+docker container rm $TEMP_CONTAINER
