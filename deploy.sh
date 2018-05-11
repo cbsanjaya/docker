d79e73744e0a3a2dd74f56af8ba994017f2eb81a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-05-12 00:21:47 +0700

    backup db dulu sebelum app di update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+docker-compose exec -T --user laravel db backup
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull app
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 docker image prune -a -f
 docker-compose exec -T --user laravel app composer run-script --quiet update-app
