6d52834df5a61c2cc3ad879443debf56ff9182ed
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-05-12 00:30:56 +0700

    bisa memberikan nama

--- backup.sh
+++ backup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-POSTFIX="`date +%Y%m%d%H%M%S`";
-FILE_NAME=$MYSQL_DATABASE-$POSTFIX.sql.gz
+if [ "$1" != '' ]; then
+    POSTFIX="`date +%Y%m%d%H%M%S`";
+    FILE_NAME=$MYSQL_DATABASE-$POSTFIX.sql.gz
+else
+    FILE_NAME=$1.sql.gz
+fi
 
 mysqldump --user=$MYSQL_USER --password=$MYSQL_PASSWORD \
     $MYSQL_DATABASE | gzip > /backup/$FILE_NAME
