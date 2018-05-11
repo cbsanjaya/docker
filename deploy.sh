6255b746a3d6082cbd5cc85aacdba7aea79505a2
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-05-11 23:07:56 +0700

    pull all image first

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 docker image prune -a -f
 docker-compose exec --user laravel app composer run-script --quiet update-app
