eacaa4f3fc95bf7ded8d7997ff5eb40edb06ab7b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-05-12 23:06:17 +0700

    backup first on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-docker-compose exec -T --user laravel db backup
+###### Run Backup First ########################################################
+/bin/sh ./backup.sh deploy
+################################################################################
+
+###### Update Application ######################################################
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull app
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --no-deps app
 docker image prune -a -f
 docker-compose exec -T --user laravel app composer run-script --quiet update-app
+################################################################################
+
+###### up Website ##############################################################
+docker-compose exec -T --user laravel app php artisan up
+################################################################################
