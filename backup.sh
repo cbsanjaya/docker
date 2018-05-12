7185384410d81c15585913ecf09d44ccaac1dd4c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-05-12 15:42:11 +0700

    restructure

--- backup.sh
+++ backup.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+FTP_HOST=ftp://localhost/volume/
+FTP_USER=user_ftp
+FTP_PASS=pass_ftp
+
 POSTFIX="`date +%Y%m%d%H%M%S`";
 FILE_NAME=volume-$POSTFIX.tar.gz
 
 docker-compose exec -T --user laravel db backup auto
 tar czvf $FILE_NAME volume
+
+curl -T $FILE_NAME $FTP_HOST --user $FTP_USER:$FTP_PASS
+
+rm $FILE_NAME
